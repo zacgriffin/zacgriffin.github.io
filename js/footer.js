4799192a9e2e142891b6d16041da83cf53fc5ec5
zacgriffin <103292845+zacgriffin@users.noreply.github.com>
2022-04-18 17:56:20 -0500

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+document.write('<div>\
+<div class="mt-5 p-4 text-black text-center">\
+  <p>&copy; Zac Griffin 2022 </p>\
+</div>\
+</div>\
+');
---
+++ js/nav.js
@@ -0,0 +1,38 @@
+document.write('<div>\
+<a href="index.html"> <img class=\"zaglogo img-fluid mx-auto d-block\" src=\"images/websitelogo.PNG\" alt=\"Zac\'s Website Logo\"> </a>\
+<nav class="navbar navbar-expand-md fw-bold navbar-light">\
+  <div class ="container-fluid">\
+    <div class="navbar-collapse collapse dual-collapse2">\
+      <ul class="navbar-nav justify-content-md-center w-100"">\
+        <li class="nav-item mx-3">\
+          <a class="nav-link" href="index.html">HOME</a>\
+        </li>\
+        <li class="navdivider">|</li>\
+        <li class="navbar-item mx-3">\
+          <a class="nav-link" href="about.html">ABOUT</a>\
+        </li>\
+        <li class="navdivider">|</li>\
+        <li class="navbar-item mx-3">\
+          <a class="nav-link" href="articles.html">ARTICLES</a>\
+        </li>\
+        <li class="navdivider">|</li>\
+        <li class="navbar-item mx-3">\
+          <a class="nav-link" href="books.html">BOOKS</a>\
+        </li>\
+        <li class="navdivider">|</li>\
+        <li class="navbar-item mx-3">\
+          <a class="nav-link" href="resources.html">RESOURCES</a>\
+        </li>\
+        <li class="navdivider">|</li>\
+        <li class="navbar-item mx-3">\
+          <a class="nav-link" href="contact.html">CONTACT</a>\
+        </li>\
+      </ul>\
+    </div>\
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".dual-collapse2">\
+      <span class="navbar-toggler-icon"></span>\
+    </button>\
+  </div>\
+</nav>\
+</div>\
+');
