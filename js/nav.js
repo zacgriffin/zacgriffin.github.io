9eea522b1b67be3902e3244b9f0831296d38efff
zacgriffin <103292845+zacgriffin@users.noreply.github.com>
2022-04-18 18:01:43 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,31 @@
 document.write('<div>\
-<a href="index.html"> <img class=\"zaglogo img-fluid mx-auto d-block\" src=\"images/websitelogo.PNG\" alt=\"Zac\'s Website Logo\"> </a>\
+<a href="index.html"> <img class=\"zaglogo img-fluid mx-auto d-block\" src=\"/images/websitelogo.PNG\" alt=\"Zac\'s Website Logo\"> </a>\
 <nav class="navbar navbar-expand-md fw-bold navbar-light">\
   <div class ="container-fluid">\
     <div class="navbar-collapse collapse dual-collapse2">\
       <ul class="navbar-nav justify-content-md-center w-100"">\
         <li class="nav-item mx-3">\
-          <a class="nav-link" href="index.html">HOME</a>\
+          <a class="nav-link" href="/index.html">HOME</a>\
         </li>\
         <li class="navdivider">|</li>\
         <li class="navbar-item mx-3">\
-          <a class="nav-link" href="about.html">ABOUT</a>\
+          <a class="nav-link" href="/about.html">ABOUT</a>\
         </li>\
         <li class="navdivider">|</li>\
         <li class="navbar-item mx-3">\
-          <a class="nav-link" href="articles.html">ARTICLES</a>\
+          <a class="nav-link" href="/articles.html">ARTICLES</a>\
         </li>\
         <li class="navdivider">|</li>\
         <li class="navbar-item mx-3">\
-          <a class="nav-link" href="books.html">BOOKS</a>\
+          <a class="nav-link" href="/books.html">BOOKS</a>\
         </li>\
         <li class="navdivider">|</li>\
         <li class="navbar-item mx-3">\
-          <a class="nav-link" href="resources.html">RESOURCES</a>\
+          <a class="nav-link" href="/resources.html">RESOURCES</a>\
         </li>\
         <li class="navdivider">|</li>\
         <li class="navbar-item mx-3">\
-          <a class="nav-link" href="contact.html">CONTACT</a>\
+          <a class="nav-link" href="/contact.html">CONTACT</a>\
         </li>\
       </ul>\
     </div>\
